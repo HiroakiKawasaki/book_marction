0b891b0aa80ae9c23f72e5b2bf7a909d2355d47f
HiroakiKawasaki <kawa_307815@icloud.com>
2020-11-05 16:32:10 +0900

    モデル・マイグレーションファイルの修正
    formオブジェクトの除去に伴い、モデル及びマイグレーションファイルを修正、削除しました。
    また、カラムの追加を行いました。

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,19 @@ class Book < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :users, optional: true
-  has_one :plans
   has_many :comments
-  belongs_to_active_hash :genre
+  belongs_to_active_hash :genre, :achievement
+
+  with_options presence: true do
+    validates :title
+    validates :author
+    validates :genre_id
+    validates :explanation
+    validates :reason
+    validates :thing
+    validates :todo
+  end
+
+  validates :genre_id, numericality: { greater_than: 0 }
+
 end
--- db/migrate/20201030023407_create_books.rb
+++ db/migrate/20201030023407_create_books.rb
@@ -2,9 +2,17 @@ class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
       t.string     :title,       null: false
+      t.string     :author,      null: false
       t.integer    :genre_id,    null: false
       t.text       :explanation, null: false
       t.references :user,        null: false, foreign_key: true
+      t.string     :reason, null: false
+      t.string     :time
+      t.string     :place
+      t.string     :partner
+      t.string     :thing, null: false
+      t.string     :todo, null: false
+      t.integer    :achievement_id
 
       t.timestamps
     end
